a1f3c60ba426dc4ca10504e872ce727bf1564167
노승남 <bigrsnboy@naver.com>
2018-07-29 21:31:08 +0900

    programmers mock test #3 not good

--- additioncycle/main.cpp
+++ additioncycle/main.cpp
@@ -12,17 +12,36 @@
 using namespace std;
 
 
-int solution(long n) {
-    long num = n;
-    int count = 0;
-    int res;
-    while(num != 0){
-        count += num % 10;
-        num /= 10;
+
+
+vector<int> solution(vector<vector<int> > v) {
+    vector<int> ans;
+
+    int ax = v[0][0];
+    int ay = v[0][1];
+    int bx = v[1][0];
+    int by = v[1][1];
+    int cx = v[2][0];
+    int cy = v[2][1];
+    
+    int dab = (ax-bx)*(ax-bx) + (ay-by) * (ay-by) ;
+    int dbc = (cx-bx)*(cx-bx) + (cy-by) * (cy-by) ;
+    int dca = (ax-cx)*(ax-cx) + (ay-cy) * (ay-cy) ;
+    
+    int maximum = max((max(dab, dbc)), dca);
+    if(maximum == dab) {
+        return {ax+bx-cx, ay+by-cy};
+    } else if (maximum == dbc) {
+        return {bx+cx-ax, by+cy-ay};
+    } else {
+        return {ax+cx-bx, ay+by-by};
     }
-    return count;
 }
 
+
 int main(int argc, const char* argv[]) {
-    cout << solution(2342354624);
+    vector<vector<int>> v = {{4,1}, {4,4}, {1,4}};
+    vector<int> res = solution(v);
+    cout << res[0] <<", " << res[1] << endl;
+    return 0;
 }
