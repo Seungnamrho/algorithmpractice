abb0bcaead11cd00d20ef088dbf98b49449b1fc7
노승남 <bigrsnboy@naver.com>
2018-07-29 18:57:57 +0900

    traversal followed book error

--- additioncycle/main.cpp
+++ additioncycle/main.cpp
@@ -10,48 +10,29 @@
 #include <vector>
 using namespace std;
 
-struct node {
-    string label;
-    node* left_child;
-    node* right_child;
-};
-
-node* construction(vector<int>& preorder, vector<int>& inorder) {
-    node root;
-    node* rootptr = &root;
-    vector<int> left_inorder;
-    vector<int> right_inorder;
-    rootptr->label = preorder[0];
-    return rootptr;
+vector<long> slice(const vector<long>& v, int a, int b) {
+    return vector<long>(v.begin() + a, v.begin() + b);
 }
 
-//original이라는 int형 array를 받아서, 시작 index~ 끝 index를 받아 sub array를 return한다.
-vector<int> subvector(vector<int> original, int start_idx, int final_idx) {
-    return vector<int>(original.begin()+start_idx, original.begin()+final_idx +1);
+void printPostOrder(const vector<long>& preorder, const vector<long>& inorder) {
+    const long N = preorder.size();
+    if(preorder.empty()) {
+        return;
+    }
+    const int root = preorder[0];
+    const long L = find(inorder.begin(), inorder.end(), root) - inorder.begin();
+    const int R = N - L - 1;
+    printPostOrder(slice(preorder, 1, L+1), slice(inorder, 0, L));
+    printPostOrder(slice(preorder, L+1, N), slice(inorder, L+1, N));
+    cout << root <<' ';
 }
 
 
 int main(int argc, const char* argv[]) {
-    vector<int> test = {1,2,3,4,5,6,7,8}, another;
-    another = subvector(test, 1, 3);
-    for(int i=0; i<another.size();i++){
-        cout << another[i];
-    }
-    int node_no;
-    cin >> node_no;
-    vector<int> preorder, inorder;
-    //temp 변수는 cin으로 사용자로부터 input을 받고 vector에 값을 넣기 위해 선언해 준 중간 전달값이라고 하겠다.
-    int temp;
-    //preorder의 배열을 받는다.
-    for(int i =0; i<node_no; i++) {
-        cin >> temp;
-        preorder.push_back(temp);
-    }
-    //inorder의 배열을 받는다.
-    for(int i =0; i<node_no; i++) {
-        cin >> temp;
-        inorder.push_back(temp);
-    }
-    construction(preorder, inorder);
-    return 0;
+    vector<long> pre = {27, 16, 9, 12, 54, 36, 72};
+    vector<long> in = {9, 12, 16, 27, 36, 54, 762};
+    printPostOrder(pre, in);
+
 }
+
+
