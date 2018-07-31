4433096be3bb3b6f071040baf0991ba97654b6d1
노승남 <bigrsnboy@naver.com>
2018-07-31 17:11:56 +0900

    땅따먹기 dynamic programming implementation

--- additioncycle/main.cpp
+++ additioncycle/main.cpp
@@ -8,44 +8,48 @@
 
 #include <iostream>
 #include <vector>
-#include <stack>
+#include <algorithm>
+#include <array>
+#include <math.h>
+
 using namespace std;
 
-
-
-
-vector<int> solution(vector<vector<int> > v) {
-    vector<int> ans;
-
-    int ax = v[0][0];
-    int ay = v[0][1];
-    int bx = v[1][0];
-    int by = v[1][1];
-    int cx = v[2][0];
-    int cy = v[2][1];
-    
-    if(ax == bx){
-        ans.push_back(cx);
-    }else if(bx == cx) {
-        ans.push_back(ax);
-    }else {
-        ans.push_back(bx);
+int solution(vector<vector<int>> board)
+{
+    unsigned long num_row = board.size();
+    unsigned long num_col = board[0].size();
+    int result = 0;
+    int dp[num_row][num_col];
+    for(int j=0; j<num_col; j++) {
+        dp[0][j] = board[0][j];
     }
     
-    if(ay == by){
-        ans.push_back(cy);
-    }else if(by == cy) {
-        ans.push_back(ay);
-    }else {
-        ans.push_back(by);
+    for(int i=1; i<num_row; i++) {
+        int maximum = 0;
+        for(int j=0; j<num_col; j++) {
+            int a, b, c;
+            a = (j+1) % 4;
+            b = (j+2) % 4;
+            c = (j+3) % 4;
+            maximum = max(max( dp[i-1][a], dp[i-1][b]), dp[i-1][c]);
+            dp[i][j] = maximum + board[i][j];
+        }
     }
-    return ans;
+    for(int j=0; j<num_col; j++) {
+        result = max(result, dp[num_row-1][j]);
+    }
+    
+    return result;
 }
-
-
+//원래 문제는 위에서 내려오는 것이지만 코드 작성의 편의성과 가독성을 위해 올라오는 것으로 문제를 임의로 변경하겠다.
 int main(int argc, const char* argv[]) {
-    vector<vector<int>> v = {{4,1}, {4,4}, {1,4}};
-    vector<int> res = solution(v);
-    cout << res[0] <<", " << res[1] << endl;
+    vector<vector<int>> board = {
+        {1, 3, 5, 7},
+        {2, 4, 6, 8},
+        {3, 2, 4, 1},
+        {1, 3, 4, 2}
+    };
+    cout << solution(board);
+  
     return 0;
 }
