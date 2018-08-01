94ef49f40a96d182b8eb6b19be3a391fe6d3e3f0
노승남 <bigrsnboy@naver.com>
2018-08-01 23:59:12 +0900

    programmers mock test #7 solution

--- additioncycle/main.cpp
+++ additioncycle/main.cpp
@@ -7,43 +7,41 @@
 //
 
 #include <iostream>
+#include <string>
 #include <vector>
+#include <set>
 #include <array>
-#include <math.h>
-
 using namespace std;
 
-int solution(vector<int> sticker)
+int solution(vector<string> strs, string t)
 {
-    int num = sticker.size();
-    int dp[num], dp2[num];
-    if(num == 1) {
-        return sticker[0];
-    } else if(num == 2) {
-        return max(sticker[0], sticker[1]);
-    } else if(num == 3) {
-        return max(max(sticker[0], sticker[1]), sticker[2]);
-    }
-    //dp는 첫째 놈을 고르지 않았을 때
-    dp[0] = 0;
-    dp[1] = sticker[1];
-    //dp2는 첫째 놈을 골랐을 때
-    dp2[0] = sticker[0];
-    dp2[1] = dp2[0];
-    dp2[num-1] = 0;
+    set<string> str_set(strs.begin(), strs.end());
+    const int INF = 987654321;
+    vector<int> dp(20002, INF);
+    int len = t.length();
 
-    for(int i=2; i<num-1; ++i) {
-        dp[i] = max(dp[i-1], dp[i-2] + sticker[i]);
-        dp2[i] = max(dp2[i-1], dp2[i-2] + sticker[i]);
-    }
-    dp[num-1] =max(dp[num-2], dp[num-3] + sticker[num-1]);
+    dp[len] = 0;
     
-    return max(dp[num-1], dp2[num-2]);
+    for(int i=len-1; i>=0; i--) {
+        string tmp = "";
+        for(int j=0; j<5 && i+j<len; j++) {
+            tmp += t[i+j];
+            if(str_set.find(tmp) != str_set.end() && dp[i+j+1] != INF) {
+                dp[i] = min(dp[i], dp[i+j+1] + 1);
+            }
+        }
+    }
+    if(dp[0] == INF) {
+        return -1;
+    }else {
+        return dp[0];
+    }
 }
 
-int main(int argc, const char* argv[]) {
-    vector<int> sticker = {5,4,3, 7};
-    
-    cout << solution(sticker);
+int main(int argc, const char * argv[]) {
+    vector<string> pieces = {"app","ap","p","l","e","ple","pp"};
+    string goal = "apple";
+    cout << solution(pieces, goal);
     return 0;
+    
 }
