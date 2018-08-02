cd2ac28c53252a63a0df3ec226c2a931f3709db6
노승남 <bigrsnboy@naver.com>
2018-08-02 16:22:22 +0900

    kakao group picture problem solved

--- additioncycle/main.cpp
+++ additioncycle/main.cpp
@@ -7,98 +7,58 @@ using namespace std;
 
 int solution(int n, vector<string> data) {
     char test2[8] =  {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
-    sort(test2, test2+8);
     int count = 0;
+    //n이 10개라 해보자.
     int num_of_conditions = n;
+    //요까지 조건 잘 들어옴.
+    //conditions = {"N~F=0", "R~T>2"};
     vector<string> conditions = data;
-    //string conditions[2] = {"J~C=1", "F~A>0"};
     do {
         for(int i=0; i<num_of_conditions; i++) {
-            long first_idx = find(test2, test2+4,conditions[i][0]) - test2;
-            long second_idx = find(test2, test2+4,conditions[i][2]) - test2;
+            long first_idx = find(test2, test2+8, conditions[i][0]) - test2;
+            long second_idx = find(test2, test2+8, conditions[i][2]) - test2;
             char op = conditions[i][3];
             int comp = conditions[i][4] - '0';
             
             if(op == '>') {
-                
                 if(abs(first_idx - second_idx) > comp+1) {
-                    
+                    //do nothing
                 } else {
                     break;
                 }
             } else if(op == '=') {
                 if(abs(first_idx - second_idx) == comp+1) {
-                    
+                    //do nothing
                 } else {
                     break;
                 }
             } else {
                 if(abs(first_idx - second_idx) < comp+1) {
-                    
+                    //do nothing
                 } else {
                     break;
                 }
             }
             
             if(i == num_of_conditions-1){
-                cout << test2[0] << ' ' << test2[1] << ' ' << test2[2] << ' ' << test2[3] << endl;
+               
                 count ++;
             }
-            
         }
-        
-    }while(next_permutation(test2, test2+4));
-    
-    cout << count << "개" << endl;
-    return 0;
+    } while(next_permutation(test2, test2+8));
+    return count;
 }
 
-
+//입력 예 -> num = 2, conds = N~F=0 띄우고 R~T>2
 int main() {
-    char test2[4] =  {'J', 'C', 'F', 'A',};
-    sort(test2, test2+4);
-    int count = 0;
-    int num_of_conditions = 2;
-    //string conditions[2] = {"J~C=1", "F~A>0"};
-    vector<string> conditions = {"J~C=1", "F~A>0"};
-//    do {
-//        for(int i=0; i<num_of_conditions; i++) {
-//            int first_idx = find(test2, test2+4,conditions[i][0]) - test2;
-//            int second_idx = find(test2, test2+4,conditions[i][2]) - test2;
-//            char op = conditions[i][3];
-//            int comp = conditions[i][4] - '0';
-//
-//            if(op == '>') {
-//
-//                if(abs(first_idx - second_idx) > comp+1) {
-//
-//                } else {
-//                    break;
-//                }
-//            } else if(op == '=') {
-//                if(abs(first_idx - second_idx) == comp+1) {
-//
-//                } else {
-//                    break;
-//                }
-//            } else {
-//                if(abs(first_idx - second_idx) < comp+1) {
-//
-//                } else {
-//                    break;
-//                }
-//            }
-//
-//            if(i == num_of_conditions-1){
-//                cout << test2[0] << ' ' << test2[1] << ' ' << test2[2] << ' ' << test2[3] << endl;
-//                count ++;
-//            }
-//
-//        }
-//
-//    }while(next_permutation(test2, test2+4));
-//
-//    cout << count << "개" << endl;
-
-    solution(2, conditions);
+    //conditions = {"N~F=0", "R~T>2"};
+    int num =0;
+    cin >> num;
+    string temp = "";
+    vector<string> conds;
+    for(int i=0; i<num; i++) {
+        cin >> temp;
+        conds.push_back(temp);
+    }
+    cout << solution(num, conds);
 }
