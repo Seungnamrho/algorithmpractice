77a05e854ed4b480035722c77158b7fb18d2f7bd
노승남 <bigrsnboy@naver.com>
2018-08-02 15:48:16 +0900

    kaka previous test group picture temp

--- additioncycle/main.cpp
+++ additioncycle/main.cpp
@@ -1,40 +1,104 @@
-//
-//  main.cpp
-//  additioncycle
-//
-//  Created by SN Rho on 29/07/2018.
-//  Copyright © 2018 SN Rho. All rights reserved.
-//
-
 #include <iostream>
-#include <string>
-#include <vector>
-#include <set>
 #include <array>
+#include <string>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-int solution(vector<string> strs, string t)
-{
-    int INF = 987654321;
-    int len = t.length();
-    vector<int> dp(t.length(), INF);
-    set<string> str_set(strs.begin(), strs.end());
-    dp[len] = 0;
-    for(int i = len-1; i>=0; i--) {
-        string temp = "";
-        for(int j=0; j<5 && i+j<len; j++) {
-            temp += t[i+j];
-            if(str_set.find(temp) != str_set.end() && dp[i+j+1] != INF) {
-                dp[i] = min(dp[i], dp[i+j+1] + 1);
+int solution(int n, vector<string> data) {
+    char test2[8] =  {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
+    sort(test2, test2+8);
+    int count = 0;
+    int num_of_conditions = n;
+    vector<string> conditions = data;
+    //string conditions[2] = {"J~C=1", "F~A>0"};
+    do {
+        for(int i=0; i<num_of_conditions; i++) {
+            long first_idx = find(test2, test2+4,conditions[i][0]) - test2;
+            long second_idx = find(test2, test2+4,conditions[i][2]) - test2;
+            char op = conditions[i][3];
+            int comp = conditions[i][4] - '0';
+            
+            if(op == '>') {
+                
+                if(abs(first_idx - second_idx) > comp+1) {
+                    
+                } else {
+                    break;
+                }
+            } else if(op == '=') {
+                if(abs(first_idx - second_idx) == comp+1) {
+                    
+                } else {
+                    break;
+                }
+            } else {
+                if(abs(first_idx - second_idx) < comp+1) {
+                    
+                } else {
+                    break;
+                }
             }
+            
+            if(i == num_of_conditions-1){
+                cout << test2[0] << ' ' << test2[1] << ' ' << test2[2] << ' ' << test2[3] << endl;
+                count ++;
+            }
+            
         }
-    }
-    return dp[0] == INF ? -1 : dp[0] ;
-}
-int main(int argc, const char * argv[]) {
-    vector<string> pieces = {"app","ap","p","l","e","ple","pp"};
-    string goal = "apple";
-    cout << solution(pieces, goal);
-    return 0;
+        
+    }while(next_permutation(test2, test2+4));
     
+    cout << count << "개" << endl;
+    return 0;
+}
+
+
+int main() {
+    char test2[4] =  {'J', 'C', 'F', 'A',};
+    sort(test2, test2+4);
+    int count = 0;
+    int num_of_conditions = 2;
+    //string conditions[2] = {"J~C=1", "F~A>0"};
+    vector<string> conditions = {"J~C=1", "F~A>0"};
+//    do {
+//        for(int i=0; i<num_of_conditions; i++) {
+//            int first_idx = find(test2, test2+4,conditions[i][0]) - test2;
+//            int second_idx = find(test2, test2+4,conditions[i][2]) - test2;
+//            char op = conditions[i][3];
+//            int comp = conditions[i][4] - '0';
+//
+//            if(op == '>') {
+//
+//                if(abs(first_idx - second_idx) > comp+1) {
+//
+//                } else {
+//                    break;
+//                }
+//            } else if(op == '=') {
+//                if(abs(first_idx - second_idx) == comp+1) {
+//
+//                } else {
+//                    break;
+//                }
+//            } else {
+//                if(abs(first_idx - second_idx) < comp+1) {
+//
+//                } else {
+//                    break;
+//                }
+//            }
+//
+//            if(i == num_of_conditions-1){
+//                cout << test2[0] << ' ' << test2[1] << ' ' << test2[2] << ' ' << test2[3] << endl;
+//                count ++;
+//            }
+//
+//        }
+//
+//    }while(next_permutation(test2, test2+4));
+//
+//    cout << count << "개" << endl;
+
+    solution(2, conditions);
 }
