bafc9a710697481e403c86f4f79665e797f31eaa
Sam Rho <seungnam2@gmail.com>
2020-01-03 16:29:45 +0900

    init: 첫 번째 문제 풀이 완료
    시공의 폭풍 문제. array에 넣어 놓고
    다음 input마다 체크

---
+++ algorithm/1909_1_1.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  algorithm
+//
+//  Created by Sam Rho on 2020/01/03.
+//  Copyright © 2020 Sam Rho. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    vector<int>::iterator it;
+    int count = 0;
+    vector<int> pool;
+    for (int i = 0; i < 4; ++i) {
+        int temp;
+        cin >> temp;
+        pool.push_back(temp);
+    }
+    for (int i = 0 ; i <5; ++i ){
+        int input;
+        cin >> input;
+        it = find(pool.begin(), pool.end(), input);
+        if (it != pool.end()) {
+            count++;
+        }
+    }
+    cout << 5 -count;
+}
---
+++ algorithm/df.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algorithm
+//
+//  Created by Sam Rho on 2020/01/03.
+//  Copyright © 2020 Sam Rho. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
