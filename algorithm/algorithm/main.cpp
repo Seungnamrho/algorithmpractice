93017e2b1cd5fb5ef6e255d5d97ce27b986bd150
Sam Rho <seungnam2@gmail.com>
2020-01-17 00:11:33 +0900

    structure: cpp 문제와 js 문제 풀이 폴더를 다르게 함
    c++ 문제도 좋지만 자바스크립트로 주언어가 변경되다시피 했기 때문에
    자바스크립트로 우선적으로 문제를 푸는 연습을 해야겠다고 생각했다.

--- algorithm/algorithm/main.cpp
+++ algorithm/algorithm/main.cpp
@@ -1,15 +1,34 @@
-//
-//  main.cpp
-//  algorithm
-//
-//  Created by Sam Rho on 2020/01/06.
-//  Copyright © 2020 Sam Rho. All rights reserved.
-//
-
+/*
+ 
+ 10 11 0 0 0 0 0 0 0 0 0
+ 0 11 10 0 0 0 0 0 0 0 0
+ 0 3 20 7 0 1 0 0 0 0 0
+ 3 0 0 9 8 0 0 0 0 0 0
+ 0 0 0 17 2 15 0 0 0 0 0
+ 0 0 13 11 0 19 2 0 0 0 0
+ 2 1 5 4 0 0 0 0 0 0 10
+ 0 0 0 0 0 0 15 14 0 0 0
+ 0 0 0 0 0 0 0 0 3 7 2
+ 0 0 0 0 0 0 9 4 1 10 15
+ 0 0 0 0 10 1 0 5 19 7 0
+ 
+ **/
 #include <iostream>
+#include <stack>
+#define SIZE 11
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    stack<int> st;
+    int arr[SIZE][SIZE];
+    for (int i = 0; i < SIZE; ++i) {
+        for (int j = 0; j < SIZE; ++j) {
+            cin >> arr[i][j];
+        }
+    }
+    
+    while (!st.empty()) {
+        
+    }
+    
 }
