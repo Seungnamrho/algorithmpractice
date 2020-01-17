a06890c1412f665ed91fbeb4dcd804ecda9d644a
Sam Rho <seungnam2@gmail.com>
2020-01-17 16:35:11 +0900

    solve: 프로그래머스 K번째 수 문제 해결

--- algorithm/js/problems/Kth_biggest.js
+++ algorithm/js/problems/Kth_biggest.js
@@ -1,17 +1,15 @@
 const arr = [1, 5, 2, 6, 3, 7, 4];
-const arr2 = [0, 0, 0];
 const commands = [
 	[2, 5, 3],
 	[4, 4, 1],
 	[1, 7, 3],
 ];
-const commands2 = [[1, 4, 2]];
 
 const ans = [5, 6, 3];
 
 function solution(array, commands) {
 	return commands.map(([start, end, K]) => {
-		const sorted = array.slice(start - 1, end).sort();
+		const sorted = array.slice(start - 1, end).sort((a, b) => a - b);
 		return sorted[K - 1];
 	});
 }
